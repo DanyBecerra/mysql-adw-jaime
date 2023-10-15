5833a88fd9bddf7ad9391f78c67ee8af076a8aaa
Daniel Andrés  Becerra Sierra <daniela_becerras@javeriana.edu.co>
2023-10-15 06:26:07 +0000

    Consulta10

--- scripts/consulta.sql
+++ scripts/consulta.sql
@@ -3,5 +3,31 @@ use adw;
 -- Empleados
 select *
 from HumanResources_Employee
-limit 5
+limit 6
 ;
+WITH produccion AS (
+    SELECT
+        p.ProductID,
+        p.Name AS Producto,
+        pc.Name AS Categoria,
+        psc.Name AS Subcategoria,
+        SUM(wo.OrderQty) AS CantidadProduccion,
+        plph.ListPrice AS CostoUnitario,
+        SUM(wo.OrderQty) * plph.ListPrice AS CostoProduccionTotal
+    FROM Production_Product AS p
+    INNER JOIN Production_ProductSubcategory AS psc ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+    INNER JOIN Production_ProductCategory AS pc ON psc.ProductCategoryID = pc.ProductCategoryID
+    INNER JOIN Production_WorkOrder AS wo ON p.ProductID = wo.ProductID
+    LEFT JOIN Production_ProductListPriceHistory AS plph ON p.ProductID = plph.ProductID
+    GROUP BY p.ProductID, p.Name, pc.Name, psc.Name, plph.ListPrice
+)
+SELECT 
+    Producto,
+    Categoria,
+    Subcategoria,
+    CantidadProduccion,
+    CostoUnitario,
+    CostoProduccionTotal
+FROM produccion
+ORDER BY CantidadProduccion DESC
+LIMIT 5;
