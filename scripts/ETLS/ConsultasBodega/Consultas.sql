b2203878b0a33ad380fa9952ffae14188d349ea6
Daniel Andrés  Becerra Sierra <daniela_becerras@javeriana.edu.co>
2023-11-17 06:39:56 +0000

    Querys

--- scripts/ETLS/ConsultasBodega/Consultas.sql
+++ scripts/ETLS/ConsultasBodega/Consultas.sql
@@ -1,29 +1,51 @@
+use WhBG;
+
+-- Muestra el nombre de los empleados que fueron contratados en el mes de enero.
+
+
 SELECT 
   e.EmployeeName, 
-  s.ShiftName, 
-  d.day_name
+  s.ShiftName as ShiftTime, 
+  d.month_name
 FROM FactHires h
-INNER JOIN DimEmployee e ON h.EmployeeID = e.EmployeeID
-INNER JOIN DimShift s ON h.ShiftID = s.ShiftID
-INNER JOIN DimDate d ON h.Date = d.date
-WHERE d.day_name = 'Monday';
+INNER JOIN DimEmployee e ON h.Employee_FK = e.Employee_Key
+INNER JOIN DimShift s ON h.Shift_FK = s.Shift_Key
+INNER JOIN Dimdate d ON h.Date_FK = d.date_key
+WHERE d.month_name = 'January'; 
 
 SELECT 
-  AgeRangeName, 
-  COUNT() AS NumberOfEmployees 
-FROM DimEmployee
-GROUP BY AgeRangeName;
+  sum(h.amount)
+FROM FactHires h
+INNER JOIN DimEmployee e ON h.Employee_FK = e.Employee_Key
+INNER JOIN DimShift s ON h.Shift_FK = s.Shift_Key
+INNER JOIN Dimdate d ON h.Date_FK = d.date_key
+WHERE d.month_name = 'January'; 
 
+
+-- Esta consulta recupera el nombre de cada título y cuenta el número de veces que cada título ha sido asociado a una contratación en la tabla FactHires. Los resultados se agrupan por el nombre del título.
 SELECT 
   et.TitleName, 
-  COUNT(*) AS NumberOfHires 
+  sum(h.amount) AS NumberOfHires
 FROM FactHires h
-INNER JOIN DimEmployeeTitle et ON h.TitleID= et.TitleID
+INNER JOIN DimEmployeeTitle et ON h.Title_FK = et.Title_Key -- Corregido para usar las claves correctas
 GROUP BY et.TitleName;
 
+-- Consulta para obtener todos los empleados con su correspondiente turno y nombre de departamento
+
 SELECT 
-  e.EmployeeName, 
-  et.DepartmentName, 
-  et.TitleName 
+  e.EmployeeName,
+  s.ShiftName,
+  et.DepartmentName
 FROM DimEmployee e
-INNER JOIN DimEmployeeTitle et ON e.EmployeeID_PK = et.TitleID_PK;
+LEFT JOIN FactHires f ON e.Employee_Key = f.Employee_FK
+LEFT JOIN DimShift s ON f.Shift_FK = s.Shift_Key
+LEFT JOIN DimEmployeeTitle et ON f.Title_FK = et.Title_Key;
+
+-- Esta consulta calcula el total de contrataciones por departamento sumando el campo amount.
+SELECT 
+  et.DepartmentName, 
+  SUM(h.amount) AS TotalHires 
+FROM FactHires h
+INNER JOIN DimEmployeeTitle et ON h.Title_FK = et.Title_Key 
+GROUP BY et.DepartmentName;
+
